990adf53be0df3043c7a3a8844de31a6e3553ebd
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-24 07:50:46 +0530

    Problem three

---
+++ Problem three.py
@@ -0,0 +1,11 @@
+numArray = map(int, raw_input().split()) # Get the input
+
+
+sum_integer = 0
+# write your logic to add these 4 numbers here
+
+for i in range(0,4):
+    sum_integer=sum_integer+numArray[i]
+
+
+print sum_integer # Print the sum
