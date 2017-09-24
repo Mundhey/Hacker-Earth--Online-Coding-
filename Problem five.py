c07fc2017955d0cf81bcbcde3d053534231d88a7
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-24 08:00:35 +0530

    Problem five

---
+++ Problem five.py
@@ -0,0 +1,14 @@
+N = input()
+
+# Get the input
+numArray = map(int, raw_input().split())
+
+sum_integer = 0
+# Write the logic to add these numbers here
+
+for i in range(0,N):
+    sum_integer=sum_integer+(numArray[i]**2)
+
+
+# Print the sum
+print sum_integer
